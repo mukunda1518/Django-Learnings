0c724d53dbf5da6078cf42f792c4db716151508b
91913 <mukundapm1518@gmail.com>
2022-09-14 19:30:12 +0530

    Hands on aggregations, Annotate

--- model_queries/models.py
+++ model_queries/models.py
@@ -41,3 +41,30 @@ class Membership(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     date_joined = models.DateField(null=True, blank=True)
     invite_reason = models.CharField(max_length=64, null=True, blank=True)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True, blank=True)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField(null=True, blank=True)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
+
+
+
